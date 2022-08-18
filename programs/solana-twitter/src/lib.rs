b1ef1002ebb057ce41655260c613cd2939e8eb4d
Gundu Bhavana Sri Lahari <laharigundu@Gundus-MacBook-Pro.local>
2022-08-18 19:13:41 +0530

    Added test for sending tweet

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -5,49 +5,39 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod solana_twitter {
     use super::*;
-
-    pub fn sendTweet(ctx: Context<SendTweet>, topic: String, content: String) -> ProgramResult {
-        let tweet: &mut Account<'info, Tweet> = ctx.accounts.tweet;
-        let author: &Signer = ctx.accounts.author;
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> Result<()> {
+        let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
+        let author: &Signer = &ctx.accounts.author;
         let clock: Clock = Clock::get().unwrap();
 
-        if topic.chars().count() > 50{
-            return Err(ErrorCode::TopicTooLong.into());
+        if topic.chars().count() > 50 {
+            return Err(ErrorCode::TopicTooLong.into())
         }
 
-        if content.chars().count() > 280{
+        if content.chars().count() > 280 {
             return Err(ErrorCode::ContentTooLong.into())
         }
 
         tweet.author = *author.key;
+        tweet.timestamp = clock.unix_timestamp;
         tweet.topic = topic;
         tweet.content = content;
-        tweet.timestamp = clock.unix_timestamp;
 
         Ok(())
     }
 }
 
-#[error_code]
-pub enum ErrorCode{
-    #[msg("Provided topic is too long. Make sure it has <= 50 characters.")]
-    TopicTooLong,
-    #[msg("Provided content is too long. Make sure it has <= 280 characters.")]
-    ContentTooLong,
-}
-
 #[derive(Accounts)]
-pub struct SendTweet<'info>{
+pub struct SendTweet<'info> {
     #[account(init, payer = author, space = Tweet::LEN)]
     pub tweet: Account<'info, Tweet>,
     #[account(mut)]
     pub author: Signer<'info>,
-    #[account(address = system_program::ID)]
-    pub system_program: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
-pub struct Tweet{
+pub struct Tweet {
     pub author: Pubkey,
     pub timestamp: i64,
     pub topic: String,
@@ -57,17 +47,22 @@ pub struct Tweet{
 const DISCRIMINATOR_LENGTH: usize = 8;
 const PUBLIC_KEY_LENGTH: usize = 32;
 const TIMESTAMP_LENGTH: usize = 8;
-const STRING_PREFIX_LENGTH: usize = 4;
-const MAX_TITLE_LENGTH: usize = 50 * 4;
-const MAX_CONTENT_LENGTH: usize = 280 * 4;
+const STRING_LENGTH_PREFIX: usize = 4; // Stores the size of the string.
+const MAX_TOPIC_LENGTH: usize = 50 * 4; // 50 chars max.
+const MAX_CONTENT_LENGTH: usize = 280 * 4; // 280 chars max.
 
-impl Tweet{
-    const LEN: usize = DISCRIMINATOR_LENGTH 
-    + PUBLIC_KEY_LENGTH
-    + TIMESTAMP_LENGTH
-    + STRING_PREFIX_LENGTH
-    + MAX_TITLE_LENGTH
-    + MAX_TITLE_LENGTH;
+impl Tweet {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH // Author.
+        + TIMESTAMP_LENGTH // Timestamp.
+        + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH // Topic.
+        + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; // Content.
 }
 
-
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided topic should be 50 characters long maximum.")]
+    TopicTooLong,
+    #[msg("The provided content should be 280 characters long maximum.")]
+    ContentTooLong,
+}
--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SolanaTwitter } from "../target/types/solana_twitter";
+import * as assert from "assert";
 
 describe("solana-twitter", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,26 @@ describe("solana-twitter", () => {
 
   const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    it('can send a new tweet', async () => {
+      // Call the "SendTweet" instruction.
+      const tweet = anchor.web3.Keypair.generate();
+      await program.rpc.sendTweet('BTS', 'ARMY loves BTS', {
+          accounts: {
+              tweet: tweet.publicKey,
+              author: program.provider.wallet.publicKey,
+              systemProgram: anchor.web3.SystemProgram.programId,
+          },
+          signers: [tweet],
+      });
+
+      // Fetch the account details of the created tweet.
+      const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+
+      // Ensure it has the right data.
+      assert.equal(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58());
+      assert.equal(tweetAccount.topic, 'BTS');
+      assert.equal(tweetAccount.content, 'ARMY loves BTS');
+      assert.ok(tweetAccount.timestamp);
   });
+
 });
