7c918843cef95d3118fdb41f5c0efcd21f5134fc
kamali-suthakaran <kamali.suthakaran@thoughtworks.com>
2022-09-14 19:03:53 +0530

    Add test case to send tweet from different author and  error handlers

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -31,4 +31,73 @@ describe("solana-twitter", () => {
       assert.ok(tweetAccount.timestamp);
   });
 
+  it('can send a new tweet without a topic', async () => {
+    // Call the "SendTweet" instruction.
+    const tweet = anchor.web3.Keypair.generate();
+    await program.rpc.sendTweet('', 'gm', {
+        accounts: {
+            tweet: tweet.publicKey,
+            author: program.provider.wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [tweet],
+    });
+
+    // Fetch the account details of the created tweet.
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+
+    // Ensure it has the right data.
+    assert.equal(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58());
+    assert.equal(tweetAccount.topic, '');
+    assert.equal(tweetAccount.content, 'gm');
+    assert.ok(tweetAccount.timestamp);
+});
+
+  it('can send a new tweet from a different author', async () => {
+    // Generate another user and airdrop them some SOL.
+    const otherUser = anchor.web3.Keypair.generate();
+    const signature = await program.provider.connection.requestAirdrop(otherUser.publicKey, 1000000000);
+    await program.provider.connection.confirmTransaction(signature);
+
+    // Call the "SendTweet" instruction on behalf of this other user.
+    const tweet = anchor.web3.Keypair.generate();
+    await program.rpc.sendTweet('J.K.Rowling', 'Harry Potter and the Prisoner of Azkaban', {
+        accounts: {
+            tweet: tweet.publicKey,
+            author: otherUser.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [otherUser, tweet],
+    });
+
+    // Fetch the account details of the created tweet.
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+
+    // Ensure it has the right data.
+    assert.equal(tweetAccount.author.toBase58(), otherUser.publicKey.toBase58());
+    assert.equal(tweetAccount.topic, 'J.K.Rowling');
+    assert.equal(tweetAccount.content, 'Harry Potter and the Prisoner of Azkaban');
+    assert.ok(tweetAccount.timestamp);
+});
+
+it('cannot provide a content with more than 280 characters', async () => {
+  try {
+      const tweet = anchor.web3.Keypair.generate();
+      const contentWith281Chars = 'x'.repeat(281);
+      await program.rpc.sendTweet("veg",contentWith281Chars, {
+          accounts: {
+              tweet: tweet.publicKey,
+              author: program.provider.wallet.publicKey,
+              systemProgram: anchor.web3.SystemProgram.programId,
+          },
+          signers: [tweet],
+      });
+  } catch (error) {
+      assert.equal(error.error.errorMessage, 'The provided content should be 280 characters long maximum.');
+      return;
+  }
+
+  assert.fail('The instruction should have failed with a 281-character content.');
+});
+
 });
