01f6a974f3041086a4de139bd0a0b7f0d14e102a
Gundu Bhavana Sri Lahari <laharigundu@Gundus-MacBook-Pro.local>
2022-09-28 20:05:16 +0530

    Add. Fetch tweets in frontend

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -1,8 +1,9 @@
 import { computed } from 'vue'
 import { useAnchorWallet } from 'solana-wallets-vue'
 import { Connection, PublicKey } from '@solana/web3.js'
-import { Provider, Program } from '@project-serum/anchor'
+import { AnchorProvider, Program } from '@project-serum/anchor'
 import idl from '../../../target/idl/solana_twitter.json'
+
 
 const programID = new PublicKey(idl.metadata.address)
 let workspace = null
@@ -12,7 +13,10 @@ export const useWorkspace = () => workspace
 export const initWorkspace = () => {
     const wallet = useAnchorWallet()
     const connection = new Connection('http://127.0.0.1:8899')
-    const provider = computed(() => new Provider(connection, wallet.value))
+    console.log("value ", wallet.value);
+    console.log("public key ", wallet.publicKey);
+    const provider = computed(() => new AnchorProvider(connection, wallet.value))
+    //const provider = computed(() => new AnchorProvider(connection, wallet.publicKey))
     const program = computed(() => new Program(idl, programID, provider.value))
 
     workspace = {
--- app/src/main.js
+++ app/src/main.js
@@ -2,6 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "solana-wallets-vue/styles.css";
 import "./main.css";
+import dayjs from 'dayjs'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+import relativeTime from 'dayjs/plugin/relativeTime'
+dayjs.extend(localizedFormat)
+dayjs.extend(relativeTime)
 import { createRouter, createWebHashHistory } from "vue-router";
 import routes from "./routes";
 const router = createRouter({
