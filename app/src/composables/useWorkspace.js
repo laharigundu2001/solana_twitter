a14f9313a936f2ddbf63c98c86fa444fbde4086b
Gundu Bhavana Sri Lahari <laharigundu@Gundus-MacBook-Pro.local>
2022-09-29 18:55:52 +0530

    Send tweets from frontend

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -5,17 +5,22 @@ import { AnchorProvider, Program } from '@project-serum/anchor'
 import idl from '../../../target/idl/solana_twitter.json'
 
 
+const preflightCommitment = 'processed'
+const commitment = 'processed'
 const programID = new PublicKey(idl.metadata.address)
 let workspace = null
+
 
 export const useWorkspace = () => workspace
 
 export const initWorkspace = () => {
     const wallet = useAnchorWallet()
-    const connection = new Connection('http://127.0.0.1:8899')
+    const connection = new Connection('http://127.0.0.1:8899', commitment)
+    const provider = computed(() => new AnchorProvider(connection, wallet.value, { preflightCommitment, commitment }))
+    // const connection = new Connection('http://127.0.0.1:8899')
     console.log("value ", wallet.value);
     console.log("public key ", wallet.publicKey);
-    const provider = computed(() => new AnchorProvider(connection, wallet.value))
+    // const provider = computed(() => new AnchorProvider(connection, wallet.value))
     //const provider = computed(() => new AnchorProvider(connection, wallet.publicKey))
     const program = computed(() => new Program(idl, programID, provider.value))
 
